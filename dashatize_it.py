cb73ebd2ff72c00e64ba74277a4134ee166a74e1
Dave J <daveswork@users.noreply.github.com>
2017-08-03 19:49:00 -0400

    First stab at Dashatize It.

---
+++ dashatize_it.py
@@ -0,0 +1,32 @@
+def dashatize(num):
+    working_list = str(num).strip("-")
+    out_list = [""]
+    interesting_char = ""
+    if num is None:
+        return "None"
+    for i in range(0, len(working_list)):
+        interesting_char = working_list[i]
+        if int(interesting_char)%2 != 0:
+            if len(working_list)>1 and i == 0:
+                out_list.append(interesting_char)
+                out_list.append("-")
+            elif i == 0:
+                out_list.append(interesting_char)
+            elif out_list[-1] == "-" and i != len(working_list) - 1:
+                out_list.append(interesting_char)
+                out_list.append("-")
+            elif out_list[-1] == "-" and i == len(working_list) - 1:
+                out_list.append(interesting_char)
+            elif i == len(working_list) -1:
+                out_list.append("-")
+                out_list.append(interesting_char)
+            else:
+                out_list.append("-")
+                out_list.append(interesting_char)
+                out_list.append("-")
+        else:
+            out_list.append(interesting_char)
+    return "".join(out_list)
+
+something = -1
+print(dashatize(something))
