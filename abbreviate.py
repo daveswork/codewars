b239203a2f6c27d84b3c30bb5471629a2ae8c79f
Dave <davescripting@gmail.com>
2017-08-03 15:34:47 -0400

    First attempt at Word a10n (abbreviation)

--- abbreviate.py
+++ abbreviate.py
@@ -1,14 +1,31 @@
-import re
-
 def abbreviate(s):
-    sentence = s.split()
     test = []
+    letter_count = 0
+    position = 0
+    word_count = 0
+    sentence = s.split()
     for word in sentence:
-        abbv = word[0]+str(len(word)-2)+word[-1]
-        test.append(abbv)
-    return ' '.join(test)
+        if len(word)>4:
+            for char in word:
+                if char.isalpha():
+                    if letter_count == 0:
+                        test.append(char)
+                    letter_count += 1
+                    last_alpha = char
+                    if position == len(word)-1:
+                        test.append(str(letter_count-2))
+                        test.append(last_alpha)
+                else:
+                    test.append(str(letter_count-2))
+                    test.append(last_alpha)
+                    letter_count = 0
+                    test.append(char)
+                position += 1
+            position = 0
+        else:
+            test.append(word)
+        if word_count != len(sentence) -1:
+            test.append(" ")
+        word_count += 1
+    return ''.join(test)
 
-
-string = "internationalization"
-
-print(abbreviate(string))
