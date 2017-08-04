0ffa197b2676774182aa4f307abe5bad9c1708b3
Dave J <daveswork@users.noreply.github.com>
2017-08-03 21:58:24 -0400

    Latest submission.

--- dashatize_it.py
+++ dashatize_it.py
@@ -7,14 +7,11 @@ def dashatize(num):
     for i in range(0, len(working_list)):
         interesting_char = working_list[i]
         if int(interesting_char)%2 != 0:
-            if len(working_list)>1 and i == 0:
+            if len(working_list)>1 and i == 0 or out_list[-1] == "-" and i != len(working_list) - 1:
                 out_list.append(interesting_char)
                 out_list.append("-")
             elif i == 0:
                 out_list.append(interesting_char)
-            elif out_list[-1] == "-" and i != len(working_list) - 1:
-                out_list.append(interesting_char)
-                out_list.append("-")
             elif out_list[-1] == "-" and i == len(working_list) - 1:
                 out_list.append(interesting_char)
             elif i == len(working_list) -1:
@@ -27,6 +24,3 @@ def dashatize(num):
         else:
             out_list.append(interesting_char)
     return "".join(out_list)
-
-something = -1
-print(dashatize(something))
